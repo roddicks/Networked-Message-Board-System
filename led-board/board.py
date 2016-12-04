76d74c10e667dca0922f8c6db520c69707c42a98
roddicks <sheldon.roddick@hotmail.com>
2016-12-03 18:21:13 -0800

    Initialize client websocket connection and setup test message board

--- led-board/board.py
+++ led-board/board.py
@@ -1,9 +1,41 @@
 #Main message board controller
 import Queue
+import Client
+import time
+from twisted.internet import reactor
+from autobahn.twisted.websocket import WebSocketClientFactory
 
-messages = [];
+messages = ["message 1", "message 2"];
+updated = False
 alert_message_buffer = Queue.Queue(3)	#Limit to 3 alert messages
 
+SERVER_ADDRESS = 'messageboard.fuzzlesoft.ca'
+SERVER_PORT = 9011
+
 #Setup
+
 #Subscribe to message board server
+factory = WebSocketClientFactory(SERVER_ADDRESS + ":" + SERVER_PORT)
+protocol = Client.Client(onMessagesReceived)
+factory.protocol = protocol
+
+reactor.connectTCP(SERVER_ADDRESS, SERVER_PORT, factory)
+reactor.run()
+
+#Set up message board
+def onMessagesReceived(payload):
+	print payload
+
+def printMessages(messages):
+	for(m in messages):
+		print m
+		time.sleep(0.25)
+	
+	if not updated
+		printMessages(messages)
+	
+	return
+		
+
+
 
