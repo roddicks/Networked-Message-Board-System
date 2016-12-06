750ca40c08c1bdf9a7fc8ecda076aa883d018107
roddicks <sheldon.roddick@hotmail.com>
2016-12-05 17:45:33 -0800

    Added space character and cleaned up character selection

--- led-board/PPMUtil.py
+++ led-board/PPMUtil.py
@@ -32,32 +32,8 @@ def write_ppm(file, bytes):
 	
 def ppm_char_dict(c):
 	chars = {
-		'A': open('font/a.ppm', 'rb'),
-		'B': open('font/b.ppm', 'rb'),
-		'C': open('font/c.ppm', 'rb'),
-		'D': open('font/d.ppm', 'rb'),
-		'E': open('font/e.ppm', 'rb'),
-		'F': open('font/f.ppm', 'rb'),
-		'G': open('font/g.ppm', 'rb'),
-		'H': open('font/h.ppm', 'rb'),
-		'I': open('font/i.ppm', 'rb'),
-		'J': open('font/j.ppm', 'rb'),
-		'K': open('font/k.ppm', 'rb'),
-		'L': open('font/l.ppm', 'rb'),
-		'M': open('font/m.ppm', 'rb'),
-		'N': open('font/n.ppm', 'rb'),
-		'O': open('font/o.ppm', 'rb'),
-		'P': open('font/p.ppm', 'rb'),
-		'Q': open('font/q.ppm', 'rb'),
-		'R': open('font/r.ppm', 'rb'),
-		'S': open('font/s.ppm', 'rb'),
-		'T': open('font/t.ppm', 'rb'),
-		'U': open('font/u.ppm', 'rb'),
-		'V': open('font/v.ppm', 'rb'),
-		'W': open('font/w.ppm', 'rb'),
-		'X': open('font/x.ppm', 'rb'),
-		'Y': open('font/y.ppm', 'rb'),
-		'Z': open('font/z.ppm', 'rb')
+		' ': open('font/space.ppm', 'rb')
+		default: open('font/' + c + '.ppm', 'rb')
 	}
 	#Just retrieve the pixel definition bytes, no headers
 	return chars.get(c).readlines()[3]
