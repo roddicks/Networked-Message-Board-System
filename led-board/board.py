6d811f308089414f3e2a15996c2594b0c3eeccf3
roddicks <sheldon.roddick@hotmail.com>
2016-12-05 19:58:57 -0800

    Try os system call

--- led-board/board.py
+++ led-board/board.py
@@ -8,6 +8,7 @@ import json
 import PPMUtil
 import WatchdogEventHandler
 from subprocess import call
+import os
 import datetime
 
 logging.basicConfig()
@@ -61,7 +62,7 @@ def printMessages(messageQueue):
 		msg = m['msg']
 		PPMUtil.text_to_ppm(msg + ".ppm", msg)	#Digest message
 		try:
-			call(["sudo ~/board-test/rpi-rgb-led-matrix/example-api-use/demo -t 10 -D 1 --led-brightness=" + str(brightness) + " --led-rows=16 " + msg + ".ppm"])
+			os.system("sudo ~/board-test/rpi-rgb-led-matrix/example-api-use/demo -t 10 -D 1 --led-brightness=" + str(brightness) + " --led-rows=16 " + msg + ".ppm")
 		except Exception:
 			pass
 		time.sleep(10)
